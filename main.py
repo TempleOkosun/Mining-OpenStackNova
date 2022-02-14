5e9617dcfa7ed56c854eefc2dd805a2d40f1079f
Temple <okosun.temple@gmail.com>
2022-02-13 19:46:16 -0500

    added jupyter notebook for data analysis

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
+import json
+import pandas as pd
+
 import requests
-from utils import extract_commits, commit_contents, commit_contents, prepare_data
+from utils import extract_commits, commit_contents, prepare_data
 
 data_row = (
     prepare_data(commit_contents(extract_commits("TempleOkosun/EVChargerReg", 12), "TempleOkosun/EVChargerReg")))
 
-
 if __name__ == '__main__':
-    for record in data_row:
-        print(record)
+    records = [record for record in data_row]
+    print("at this point oprint record: " + "\n")
+    print(records)
+    with open('data.json', 'w') as outputfile:
+        json.dump(records, outputfile)
+        # for record in data_row:
+        #     json.dump(record, outputfile)
+
+
+
+
+    # with open('data.json', 'w') as outputfile:
+    #     outputfile.write("[")
+    #     for record in data_row:
+    #         outputfile.write(str(record) + "," + "\n")
+    #     outputfile.write("]")
+
+
+
+            # json.dump(record, outputfile, indent=" ")
+
 
